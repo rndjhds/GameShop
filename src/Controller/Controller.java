b72be2f8f4730f79b751b3a8622cc5352028231a
wodls1616 <wodls1616@gmail.com>
2023-07-31 17:15:12 +0900

    testbranch 생성

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -28,7 +28,7 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("aaaa");
+		System.out.println("test");
 	}
 
 	/**
